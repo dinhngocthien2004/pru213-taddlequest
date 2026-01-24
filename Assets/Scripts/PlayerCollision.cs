bb02389d22bbb060f63ec665e29ba9e9e42c96c9
thien-dev <dinhngocthien2004@gmail.com>
2026-01-24 22:34:10 +0700

    Build the Enemy

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -19,6 +19,10 @@ public class PlayerCollision : MonoBehaviour
         {
             gameManager.GameOver();
         }
+        else if (collision.CompareTag("Enemy"))
+        {
+            gameManager.GameOver();
+        }
     }
 
 }
