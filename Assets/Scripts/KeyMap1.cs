61f00b4c60418d548a06ab8e32f6f4bbcddec2c4
HoangGiang310 <kenzakishyina@gmail.com>
2026-02-05 10:50:50 +0700

    Map 2

---
+++ Assets/Scripts/KeyMap1.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeyMap1 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // Chuyển sang Scene 2
+            SceneManager.LoadScene("Game2");
+        }
+    }
+}
